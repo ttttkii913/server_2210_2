c9c4a839846e7b28ca305045c0d444d517fd88ce
ttttkii913 <dbgkdud821@daum.net>
2024-04-24 00:19:30 +0900

    Main.java 수정3

--- src/server_2210_2/Main.java
+++ src/server_2210_2/Main.java
@@ -2,7 +2,7 @@ package server_2210_2;
 
 public class Main {
     public static void main(String[] args) {
-        String s = "hello world";
+        String s = "안녕하세요";
         System.out.println(s);
     }
 }
